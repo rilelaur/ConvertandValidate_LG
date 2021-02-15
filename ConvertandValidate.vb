6bb3df64e8f5c0d15818a5e5a8a48105329e00dd
Laura <laurakriley01@gmail.com>
2021-02-14 20:11:42 -0700

    Add project files.

---
+++ ConvertandValidate.vb
@@ -0,0 +1,30 @@
+﻿'Laura Riley
+'RCET0265
+'Spring 2021
+'ConvertAndValidate
+'
+
+Option Explicit On
+Option Strict On
+
+'The program will convert a string to a 32 bit integer
+Module ConvertandValidate
+
+    Sub Main()
+        Console.WriteLine(ValidateAndConvert("a", 5))
+    End Sub
+
+    Private Function ValidateAndConvert(convertThisString As String, toThisInteger As Integer) As Integer
+        Static message$
+
+        Try
+
+        Catch ex As Exception
+
+        End Try
+
+        Return message$
+
+    End Function
+
+End Module
