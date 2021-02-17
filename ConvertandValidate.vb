30e5f61194518d073e666b5638667974164bebe9
Laura <laurakriley01@gmail.com>
2021-02-17 08:40:51 -0700

    add comments and get code to work Had this working but it didn't push code for some reason

--- ConvertandValidate.vb
+++ ConvertandValidate.vb
@@ -2,7 +2,7 @@
 'RCET0265
 'Spring 2021
 'ConvertAndValidate
-'
+'https://github.com/rilelaur/ConvertandValidate_LG.git
 
 Option Explicit On
 Option Strict On
@@ -11,20 +11,37 @@ Option Strict On
 Module ConvertandValidate
 
     Sub Main()
-        Console.WriteLine(ValidateAndConvert("a", 5))
+        Dim userinput As String
+        Dim validate As String
+        Dim converted As Integer
+
+        userinput = Console.ReadLine()
+        validate = ValidateAndConvert(userinput, converted)
+
+        If validate = "" Then
+            Console.WriteLine(converted)
+        Else
+            Console.WriteLine(validate)
+        End If
+        Console.ReadLine()
     End Sub
 
-    Private Function ValidateAndConvert(convertThisString As String, toThisInteger As Integer) As Integer
-        Static message$
-
+    Private Function ValidateAndConvert(convertThisString As String, toThisInteger As Integer) As String
+        'Converts the string to an integer
+        'If it converts it will return a blank
+        'If it fails to convert it will check if it is empty
+        'or if it is simply unable to convert and return the message acccordingly
         Try
-
+            toThisInteger = CInt(convertThisString)
+            Return " "
         Catch ex As Exception
-
+            Select Case convertThisString
+                Case ""
+                    Return "Is Empty"
+                Case Else
+                    Return "Must contain a number"
+            End Select
         End Try
-
-        Return message$
-
     End Function
 
 End Module
